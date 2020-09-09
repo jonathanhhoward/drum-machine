41674c719ba4eeeb865574152164d82f25c0afad
jonathanhhoward <2773262@gmail.com>
2020-09-09 17:41:32 -0500

    Inline const output as parameter

--- src/components/DrumDisplay.js
+++ src/components/DrumDisplay.js
@@ -2,9 +2,7 @@ import React from 'react'
 import Container from 'react-bootstrap/Container'
 import Card from 'react-bootstrap/Card'
 
-function DrumDisplay (props) {
-  const output = props.output
-
+function DrumDisplay ({ output }) {
   return (
     <Container className="h-25 p-1" fluid>
       <Card className="bg-info h-100 rounded-lg text-center" id="display">
