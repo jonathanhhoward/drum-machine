a4088ca6e3c0274bb42a713ddd6bad8a0e09a614
jonathanhhoward <2773262@gmail.com>
2020-09-09 18:14:48 -0500

    Inline constants

--- src/components/DrumPad.js
+++ src/components/DrumPad.js
@@ -1,18 +1,22 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button'
 
-function DrumPad (props) {
-  const { onClick, pad } = props
-  const PATH = process.env.PUBLIC_URL + '/sounds/'
-  const WAV = '.wav'
-
+function DrumPad ({ pad, onClick }) {
   return (
     <Button
-      block className="drum-pad h-100 p-0 rounded-lg" id={pad.name}
-      onClick={onClick} value={pad.name} variant="info"
+      block
+      className="drum-pad h-100 p-0 rounded-lg"
+      id={pad.name}
+      onClick={onClick}
+      value={pad.name}
+      variant="info"
     >
       {pad.key}
-      <audio className="clip" id={pad.key} src={PATH + pad.name + WAV}/>
+      <audio
+        className="clip"
+        id={pad.key}
+        src={`${process.env.PUBLIC_URL}/sounds/${pad.name}.wav`}
+      />
     </Button>
   )
 }
