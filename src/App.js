c34d7cd78c9cf89a59caab26be5273d58d39fd94
jonathanhhoward <2773262@gmail.com>
2020-09-09 18:58:30 -0500

    Refactor methods

--- src/App.js
+++ src/App.js
@@ -19,38 +19,29 @@ class App extends React.Component {
     document.removeEventListener('keydown', this.handleKeyDown)
   }
 
-  playAudio = (audioElem) => {
-    audioElem.play().catch(err => console.log(err))
+  handleKeyDown = ({ key }) => {
+    const KEY = key.toUpperCase()
+
+    if (!['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C'].includes(KEY))
+      return
+
+    const audioElem = document.getElementById(KEY)
+    const button = audioElem.closest('.drum-pad')
+    this.setState({ output: button.value })
+    audioElem.play()
   }
 
-  handleKeyDown = (event) => {
-    const DRUM_PAD = '.drum-pad'
-    const VALID_KEYS = ['Q', 'W', 'E', 'A', 'S', 'D', 'Z', 'X', 'C']
-    const key = event.key.toUpperCase()
-
-    if (VALID_KEYS.includes(key)) {
-      const element = document.getElementById(key)
-      const parent = element.closest(DRUM_PAD)
-      this.setState({ output: parent.value })
-      this.playAudio(element)
-    }
-  }
-
-  handleClick = (event) => {
-    const CLIP = '.clip'
-    const element = event.target
-    this.setState({ output: element.value })
-    this.playAudio(element.querySelector(CLIP))
+  handleClick = ({ target: button }) => {
+    const audioElem = button.querySelector('.clip')
+    this.setState({ output: button.value })
+    audioElem.play()
   }
 
   render () {
-    const handleClick = this.handleClick
-    const output = this.state.output
-
     return (
       <Container className="bg-dark m-0 p-1 vh-100" fluid id="drum-machine">
-        <DrumDisplay output={output}/>
-        <DrumSet onClick={handleClick}/>
+        <DrumDisplay output={this.state.output}/>
+        <DrumSet onClick={this.handleClick}/>
       </Container>
     )
   }
